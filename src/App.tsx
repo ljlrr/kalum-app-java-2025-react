a4c4f51e62d3b2cb8cb8d7f971eeeef28552ae34
unknown <ljlrr_16@hotmail.com>
2025-09-20 18:00:15 -0600

    agregando funcionalidad de dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
-
+import { CssBaseline } from '@mui/material'
 import './App.css'
+import { useState } from 'react'
+import { AppBarMenu } from './components/layout/AppBarMenu';
+import { SideNav } from './components/layout/SideNav';
 
 function App() {
   
+const [drawerOpen, setDrawerOpen] = useState(false);
+const handleDrawerToggle = () => setDrawerOpen(!drawerOpen);
+
+const handleLogout = () => {
+  console.log('Sesion cerrada');
+}
 
   return (
     <>
-     kalum-app
+     <CssBaseline/>
+     <AppBarMenu onMenuClick={handleDrawerToggle} onLogout={handleLogout}></AppBarMenu>
+      <SideNav open={drawerOpen} onClose={handleDrawerToggle}></SideNav>
     </>
   )
 }
