217365d46e2b1a48e342905615d121688019c11f
unknown <ljlrr_16@hotmail.com>
2025-11-08 17:04:02 -0600

    Actualización de hooks y store

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,11 +1,11 @@
 import {useDispatch, useSelector} from 'react-redux'
-import type { AppDispath, RootState } from '../store/store'
+import type { AppDispatch, RootState } from '../store/store'
 import { loginFailure, loginStart, loginSuccess, logout } from '../store/slices/auth/authSlice';
 import { authService } from '../components/services/authService';
 
 export const useAuth = () => {
     const {user, token, loading, error} = useSelector((state: RootState) => state.auth)
-    const dispatch = useDispatch<AppDispath>();
+    const dispatch = useDispatch<AppDispatch>();
 
     const handlerLogin = async (username: string, password:string) => {
         dispatch(loginStart());
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,12 +1,12 @@
 import { useDispatch, useSelector } from "react-redux"
-import type { AppDispath, RootState } from "../store/store";
+import type { AppDispatch, RootState } from "../store/store";
 //import {userService} from '../services/useService';
 import {userService} from '../components/services/useServices';
 
 
 export const useUser = () => {
     const {users} = useSelector((state: RootState) => state.users);
-    const dispatch = useDispatch<AppDispath>();
+    const dispatch = useDispatch<AppDispatch>();
 
     const getUsers = async () => {
         const response = await userService.findAll();
